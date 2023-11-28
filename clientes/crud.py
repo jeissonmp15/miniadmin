88a00e4d8cefedb45cd25f0d1f8694a1800697f4
Jeisson Felipe Manrique Pulido <jeissonmp15@gmail.com>
2023-11-28 11:13:29 -0500

    Se finaliza CRUD de clientes

--- clientes/crud.py
+++ clientes/crud.py
@@ -1,7 +1,8 @@
-from re import S
+from http import client
 
 
 class Clientes:
+    pk: int = None
     nombre: str = None
     nit: str = None
     nombre_contacto: str = None
@@ -14,7 +15,7 @@ class Clientes:
         return self.nombre
 
 
-    def __init__(self, nombre, nit) -> None:
+    def __init__(self, nombre=None, nit=None) -> None:
         self.nombre = nombre
         self.nit = nit
 
@@ -25,20 +26,61 @@ class Clientes:
             VALUES {self.nombre, self.nit, self.nombre_contacto, self.telefono, self.direccion, self.correo}
         """
 
-        print(query)
-
         cursor.execute(query)
         _id = cursor.lastrowid
         conn.commit()
 
         return _id
-        
 
-    def list(self):
-        pass
 
-    def retrieve(self, pk):
-        pass
+    @staticmethod
+    def list(cursor):
+        query = "SELECT * FROM Clientes"
+        cursor.execute(query)
+        clientes_data = cursor.fetchall()
+        clientes = []
+        for cliente in clientes_data:
+            kwargs = {
+                "nombre": cliente[1],
+                "nit": cliente[2], 
+            }
+            instancia_cliente = Clientes(**kwargs)
+            instancia_cliente.nombre_contacto = cliente[3]
+            instancia_cliente.telefono = cliente[4]
+            instancia_cliente.direccion = cliente[5]
+            instancia_cliente.correo = cliente[6]
 
-    def delete(self, pk):
-        pass
+            clientes.append(instancia_cliente)
+
+        return clientes
+
+
+    def retrieve(self, cursor, pk):
+        query = f"""SELECT * FROM Clientes WHERE id = {pk} LIMIT 1"""
+        cursor.execute(query)
+        cliente_data = cursor.fetchone()
+        self.pk = cliente_data[0]
+        self.nombre = cliente_data[1]
+        self.nit = cliente_data[2]
+        self.nombre_contacto = cliente_data[3]
+        self.telefono = cliente_data[4]
+        self.direccion = cliente_data[5]
+        self.correo = cliente_data[6]
+
+
+    def update(self, conn, cursor, valores):
+        query = f"""
+            UPDATE Clientes SET nombre = '{valores['nombre']}', nit = '{valores['nit']}', contacto = '{valores['contacto']}', 
+            correo = '{valores['correo']}'
+            WHERE id = {self.pk}
+        """
+        cursor.execute(query)
+        conn.commit()
+
+        self.retrieve(cursor, self.pk)
+
+
+    def delete(self, conn, cursor):
+        query = f"DELETE FROM Clientes WHERE id = {self.pk}"
+        cursor.execute(query)
+        conn.commit()
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+from pydoc import cli
 from clientes.crud import Clientes
 from connection import connect_db, close_connection
 
 
 if __name__ == '__main__':
+
     conn = connect_db()
     cursor = conn.cursor()
 
     _request = input("Escoge una opcion:\r\n 1: Clientes \r\n 2: Activos \r\n 3: Equipos \r\n")
 
     if _request == '1':
-        _request = input("Escoge una opcion:\r\n 1: Crear \r\n 2: Listar \r\n")
+        _request = input("Escoge una opcion:\r\n 1: Crear \r\n 2: Listar \r\n 3: Detalle \r\n 4: Actualizar \r\n 5: Eliminar\r\n1")
 
         if _request == '1':
             cliente = Clientes(nombre='Brinks', nit='34235245') # Obligatorio __init__
@@ -21,5 +23,53 @@ if __name__ == '__main__':
             _id = cliente.create(cursor, conn)
             print(f'El id del cliente {cliente} es {_id}')
 
+        elif _request == '2':
+            clientes = Clientes.list(cursor)
+            for cliente in clientes:
+                print(f"""
+                    Nombre: {cliente.nombre} \r\n
+                    NIT: {cliente.nit} \r\n
+                    Contacto: {cliente.nombre_contacto} \r\n
+                    Teléfono: {cliente.telefono} \r\n
+                    Dirección: {cliente.direccion} \r\n
+                    Correo: {cliente.correo} \r\n
+                    """
+                    )
+
+        elif _request == '3':
+            pk = 5
+            cliente = Clientes()
+            cliente.retrieve(cursor, pk)
+            print(cliente)
+            print(f"""
+                    Nombre: {cliente.nombre} \r\n
+                    NIT: {cliente.nit} \r\n
+                    Contacto: {cliente.nombre_contacto} \r\n
+                    Teléfono: {cliente.telefono} \r\n
+                    Dirección: {cliente.direccion} \r\n
+                    Correo: {cliente.correo} \r\n
+                    """
+                    )
+        
+        elif _request == '4':
+            pk = 5
+            cliente = Clientes()
+            cliente.retrieve(cursor, pk)
+            valores = {
+                "nombre": "Fase Pi",
+                "nit": '098765',
+                "contacto": "Ricardo Trujillo",
+                "correo": "ricardotrujillo@transer.com"
+            }
+
+            cliente.update(conn, cursor, valores)
+
+        elif _request == '5':
+            pk = 9
+            cliente = Clientes()
+            cliente.retrieve(cursor, pk)
+            cliente.delete(conn, cursor)
+
+        
         
     close_connection(cursor, conn)
